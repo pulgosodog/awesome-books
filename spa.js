fe674fc63e379f6b6d0e6dc9be74d2cb43921d0c
Gabriel Y <gabriel.yrigoyen43@gmail.com>
2023-06-10 21:39:46 -0600

    Fixing spa.js linters

--- spa.js
+++ spa.js
@@ -3,7 +3,7 @@ function addNew(element) {
   document.getElementById('add-new').style.display = 'flex';
   document.getElementById('library').style.display = 'none';
   document.getElementById('contact').style.display = 'none';
-  document.querySelectorAll('.menu-item').forEach((n) => { n.style.color = 'black' });
+  document.querySelectorAll('.menu-item').forEach((n) => { n.style.color = 'black'; });
   element.style.color = '#5c7c99';
 }
 
@@ -12,7 +12,7 @@ function list(element) {
   document.getElementById('library').style.display = 'flex';
   document.getElementById('add-new').style.display = 'none';
   document.getElementById('contact').style.display = 'none';
-  document.querySelectorAll('.menu-item').forEach((n) => { n.style.color = 'black' });
+  document.querySelectorAll('.menu-item').forEach((n) => { n.style.color = 'black'; });
   element.style.color = '#5c7c99';
 }
 
@@ -21,6 +21,6 @@ function contact(element) {
   document.getElementById('contact').style.display = 'flex';
   document.getElementById('library').style.display = 'none';
   document.getElementById('add-new').style.display = 'none';
-  document.querySelectorAll('.menu-item').forEach((n) => { n.style.color = 'black' });
+  document.querySelectorAll('.menu-item').forEach((n) => { n.style.color = 'black'; });
   element.style.color = '#5c7c99';
 }
