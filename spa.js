d261ed4fae91dadef6bca0777b6c86d222c76115
Gabriel Y <gabriel.yrigoyen43@gmail.com>
2023-06-10 21:37:30 -0600

    Fix linters mark issues
    Using // eslint-disable-next-line for functions since EsLint is marking it as no-unused-vars (It is used on the .html file)

--- spa.js
+++ spa.js
@@ -1,24 +1,26 @@
-
+// eslint-disable-next-line
 function addNew(element) {
   document.getElementById('add-new').style.display = 'flex';
   document.getElementById('library').style.display = 'none';
   document.getElementById('contact').style.display = 'none';
-  document.querySelectorAll('.menu-item').forEach((n) => { n.style.color = 'black' })
+  document.querySelectorAll('.menu-item').forEach((n) => { n.style.color = 'black' });
   element.style.color = '#5c7c99';
 }
 
+// eslint-disable-next-line
 function list(element) {
   document.getElementById('library').style.display = 'flex';
   document.getElementById('add-new').style.display = 'none';
   document.getElementById('contact').style.display = 'none';
-  document.querySelectorAll('.menu-item').forEach((n) => { n.style.color = 'black' })
+  document.querySelectorAll('.menu-item').forEach((n) => { n.style.color = 'black' });
   element.style.color = '#5c7c99';
 }
 
+// eslint-disable-next-line
 function contact(element) {
   document.getElementById('contact').style.display = 'flex';
   document.getElementById('library').style.display = 'none';
   document.getElementById('add-new').style.display = 'none';
-  document.querySelectorAll('.menu-item').forEach((n) => { n.style.color = 'black' })
+  document.querySelectorAll('.menu-item').forEach((n) => { n.style.color = 'black' });
   element.style.color = '#5c7c99';
 }
