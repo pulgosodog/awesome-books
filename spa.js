a685982773c565a83ddfcbd0c4b41a2402ef164b
Gabriel Y <gabriel.yrigoyen43@gmail.com>
2023-06-10 21:33:11 -0600

    Fix spa.js and style.css

--- spa.js
+++ spa.js
@@ -1,24 +1,24 @@
 
 function addNew(element) {
-    document.getElementById("add-new").style.display = "flex";
-    document.getElementById("library").style.display = "none";
-    document.getElementById("contact").style.display = "none";
-    document.querySelectorAll(".menu-item").forEach((n)=>{n.style.color="black"})
-    element.style.color = "#5c7c99";
+  document.getElementById('add-new').style.display = 'flex';
+  document.getElementById('library').style.display = 'none';
+  document.getElementById('contact').style.display = 'none';
+  document.querySelectorAll('.menu-item').forEach((n) => { n.style.color = 'black' })
+  element.style.color = '#5c7c99';
 }
 
 function list(element) {
-    document.getElementById("library").style.display = "flex";
-    document.getElementById("add-new").style.display = "none";
-    document.getElementById("contact").style.display = "none";
-    document.querySelectorAll(".menu-item").forEach((n)=>{n.style.color="black"})
-    element.style.color = "#5c7c99";
+  document.getElementById('library').style.display = 'flex';
+  document.getElementById('add-new').style.display = 'none';
+  document.getElementById('contact').style.display = 'none';
+  document.querySelectorAll('.menu-item').forEach((n) => { n.style.color = 'black' })
+  element.style.color = '#5c7c99';
 }
 
 function contact(element) {
-    document.getElementById("contact").style.display = "flex";
-    document.getElementById("library").style.display = "none";
-    document.getElementById("add-new").style.display = "none";
-    document.querySelectorAll(".menu-item").forEach((n)=>{n.style.color="black"})
-    element.style.color = "#5c7c99";
+  document.getElementById('contact').style.display = 'flex';
+  document.getElementById('library').style.display = 'none';
+  document.getElementById('add-new').style.display = 'none';
+  document.querySelectorAll('.menu-item').forEach((n) => { n.style.color = 'black' })
+  element.style.color = '#5c7c99';
 }
