8605b352c2ee24fd399ea2b5b81d7a7f7633fbe6
Gabriel Y <gabriel.yrigoyen43@gmail.com>
2023-06-10 21:10:51 -0600

    Add change in color on the navigation
    Based on the current displayed section, added color to the corresponding section in the navigation bar.
    Change the copyright section for a footer.

--- spa.js
+++ spa.js
@@ -1,17 +1,24 @@
-function addNew() {
+
+function addNew(element) {
     document.getElementById("add-new").style.display = "flex";
     document.getElementById("library").style.display = "none";
     document.getElementById("contact").style.display = "none";
+    document.querySelectorAll(".menu-item").forEach((n)=>{n.style.color="black"})
+    element.style.color = "#5c7c99";
 }
 
-function list() {
+function list(element) {
     document.getElementById("library").style.display = "flex";
     document.getElementById("add-new").style.display = "none";
     document.getElementById("contact").style.display = "none";
+    document.querySelectorAll(".menu-item").forEach((n)=>{n.style.color="black"})
+    element.style.color = "#5c7c99";
 }
 
-function contact() {
+function contact(element) {
     document.getElementById("contact").style.display = "flex";
     document.getElementById("library").style.display = "none";
     document.getElementById("add-new").style.display = "none";
+    document.querySelectorAll(".menu-item").forEach((n)=>{n.style.color="black"})
+    element.style.color = "#5c7c99";
 }
